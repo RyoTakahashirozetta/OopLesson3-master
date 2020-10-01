a5de7339946894c09a4d4b1650ec4be4eabb7f34
諒 髙橋 <31044@ojs.ac.jp>
2020-10-01 10:38:03 +0900

    P117  リスト4.28

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -1,25 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Chapter4 {
     class Program {
         static void Main(string[] args) {
-            string code = "12345";
+            Console.WriteLine(GetProduct());
 
-            var massage = GetMassage(code) ?? DefaultMassage();
-            Console.WriteLine(massage);
         }
 
-        //スタブ
-        private static object DefaultMassage() {
-            return "DefaultMassage";
+        private static string GetProduct() {
+            Sale sale = new Sale
+            {
+                ShopName = "pet store",
+                Amount = 100000,
+                Product = "food"
+            };
+            sale = null;
+            return sale?.Product;
         }
-        //スタブ
-        private static object GetMassage(string code) {
-            return "123";
-        }
+        
+    }
+
+    class Sale {
+        //店舗名
+        public string ShopName { get; set; }
+        //売上高
+        public int Amount { get; set; }
+        public string Product { get; set; }
     }
 }
