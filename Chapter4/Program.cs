31737e9b619604388dfa78f3b08e013849dd9f4b
諒 髙橋 <31044@ojs.ac.jp>
2020-10-01 10:21:21 +0900

    P116  リスト4.27

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -7,6 +7,19 @@ using System.Threading.Tasks;
 namespace Chapter4 {
     class Program {
         static void Main(string[] args) {
+            string code = "12345";
+
+            var massage = GetMassage(code) ?? DefaultMassage();
+            Console.WriteLine(massage);
+        }
+
+        //スタブ
+        private static object DefaultMassage() {
+            return "DefaultMassage";
+        }
+        //スタブ
+        private static object GetMassage(string code) {
+            return "123";
         }
     }
 }
