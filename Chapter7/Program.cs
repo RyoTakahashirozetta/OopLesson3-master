ea7295e0a2fe1c65e103f327de743311fbc0f7ed
諒 髙橋 <31044@ojs.ac.jp>
2020-10-15 11:06:33 +0900

    辞書登録プログラム

--- Chapter7/Program.cs
+++ Chapter7/Program.cs
@@ -10,35 +10,40 @@ namespace Chapter7 {
     class Program {
         static void Main(string[] args) {
 
-            //var lines = File.ReadAllLines("sample.txt");
-            //var we = new WordsExtractor(lines);
-            //foreach (var word in we.Extract()) {
-            //    Console.WriteLine(word);
-            //}
-            DuplicateKeySample();
-        }
+            var dict = new Dictionary<string, List<string>>();
+            Console.WriteLine("**********************");
+            Console.WriteLine("* 辞書登録プログラム *");
+            Console.WriteLine("**********************");
 
-        static public void DuplicateKeySample() {
-            // ディクショナリの初期化
-            var dict = new Dictionary<string, List<string>>() {
-               { "PC", new List<string> { "パーソナル コンピュータ", "プログラム カウンタ", } },
-               { "CD", new List<string> { "コンパクト ディスク", "キャッシュ ディスペンサー", } },
-            };
+            while (true) {
+                Console.WriteLine("1.登録 2.内容を表示 3.終了");
+                var select = Console.ReadLine();
+                if (select == "1") {
+                    Console.Write("\nKEYを入力：");
+                    var key = Console.ReadLine();
 
-            // ディクショナリに追加
-            var key = "EC";
-            var value = "電子商取引";
-            if (dict.ContainsKey(key)) {
-                dict[key].Add(value);
-            }
-            else {
-                dict[key] = new List<string> { value };
-            }
+                    Console.Write("VALUEを入力：");
+                    var value = Console.ReadLine();
 
-            // ディクショナリの内容を列挙
-            foreach (var item in dict) {
-                foreach (var term in item.Value) {
-                    Console.WriteLine("{0} : {1}", item.Key, term);
+                    if (dict.ContainsKey(key)) {
+                        dict[key].Add(value);
+                    }
+                    else {
+                        dict[key] = new List<string> { value };
+                    }
+
+                    Console.WriteLine("登録しました!");
+                    Console.WriteLine();
+                }
+                else if (select == "2") {
+                    foreach (var item in dict) {
+                        foreach (var term in item.Value) {
+                            Console.WriteLine("{0}:{1}", item.Key, term);
+                        }
+                    }
+                }
+                else if(select == "3"){
+                    Environment.Exit(0);
                 }
             }
         }
