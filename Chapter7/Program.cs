b27a4d4046d062cd3bc54d91f278e737a72cc56f
諒 髙橋 <31044@ojs.ac.jp>
2020-10-16 14:41:09 +0900

    問題7.1.2

--- Chapter7/Program.cs
+++ Chapter7/Program.cs
@@ -11,8 +11,10 @@ namespace Chapter7 {
         static void Main(string[] args) {
 
             var text = "Cozy lummox gives smart squid who asks for job pen";
+            Console.WriteLine("問題7.1.1");
             Exercise1_1(text); //問題7.1.1
-
+            Console.WriteLine("\n問題7.1.2");
+            Exercise1_2(text);//問題7.1.2
 
         }
         static void Exercise1_1(string text) {
@@ -33,6 +35,23 @@ namespace Chapter7 {
                 Console.WriteLine($"'{sort.Key}'：{sort.Value}");
             }
              
+        }
+
+        static void Exercise1_2(string text) {
+            var dict = new SortedDictionary<char, int>();
+            foreach (var t in text.ToUpper()) {
+                if ('A' <= t && t <= 'Z') {
+                    if (dict.ContainsKey(t)) {
+                        dict[t]++;
+                    }
+                    else {
+                        dict[t] = 1;
+                    }
+                }
+            }
+            foreach (var sort in dict) {
+                Console.WriteLine($"'{sort.Key}'：{sort.Value}");
+            }
         }
     }
 }
