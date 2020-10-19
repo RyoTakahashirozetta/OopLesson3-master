05694aa57dff7fd64c0a2f7bc34aa13c450ac0f0
諒 髙橋 <31044@ojs.ac.jp>
2020-10-19 13:36:04 +0900

    問題7.2

--- Chapter7/Program.cs
+++ Chapter7/Program.cs
@@ -10,48 +10,48 @@ namespace Chapter7 {
     class Program {
         static void Main(string[] args) {
 
-            var text = "Cozy lummox gives smart squid who asks for job pen";
-            Console.WriteLine("問題7.1.1");
-            Exercise1_1(text); //問題7.1.1
-            Console.WriteLine("\n問題7.1.2");
-            Exercise1_2(text);//問題7.1.2
+            // コンストラクタ呼び出し
+            var abbrs = new Abbreviations();
 
-        }
-        static void Exercise1_1(string text) {
-            var dict = new Dictionary<char, int>();
+            //問題7.2.3
+            //Countプロパティを呼び出して数を出力させる
+            Console.WriteLine(abbrs.Count);
+            Console.WriteLine(abbrs.Remove("APEC")?"成功":"失敗");
 
-            foreach (var t in text.ToUpper()) {
-                if ('A' <= t && t <= 'Z') {
-                    if (dict.ContainsKey(t)) {
-                        dict[t]++;
-                    }
-                    else {
-                        dict[t] = 1;
-                    }
-                }
-            }
-            var sorts = dict.OrderBy(s => s.Key);
-            foreach (var sort in sorts) {
-                Console.WriteLine($"'{sort.Key}'：{sort.Value}");
-            }
-             
-        }
 
-        static void Exercise1_2(string text) {
-            var dict = new SortedDictionary<char, int>();
-            foreach (var t in text.ToUpper()) {
-                if ('A' <= t && t <= 'Z') {
-                    if (dict.ContainsKey(t)) {
-                        dict[t]++;
-                    }
-                    else {
-                        dict[t] = 1;
-                    }
-                }
+
+            // インデクサの利用例
+            var names = new[] { "WHO", "FIFA", "NPT", };
+            foreach (var name in names) {
+                var fullname = abbrs[name];
+                if (fullname == null)
+                    Console.WriteLine("{0}は見つかりません", name);
+                else
+                    Console.WriteLine("{0}={1}", name, fullname);
             }
-            foreach (var sort in dict) {
-                Console.WriteLine($"'{sort.Key}'：{sort.Value}");
+            Console.WriteLine();
+
+            // ToAbbreviationメソッドの利用例
+            var japanese = "東南アジア諸国連合";
+            var abbreviation = abbrs.ToAbbreviation(japanese);
+            if (abbreviation == null)
+                Console.WriteLine("{0} は見つかりません", japanese);
+            else
+                Console.WriteLine("「{0}」の略語は {1} です", japanese, abbreviation);
+            Console.WriteLine();
+
+            // FindAllメソッドの利用例
+            foreach (var item in abbrs.FindAll("国際")) {
+                Console.WriteLine("{0}={1}", item.Key, item.Value);
             }
+            Console.WriteLine();
+
+            
+
+            foreach (var item in abbrs.Where(x => x.Key.Length == 3)) {
+                Console.WriteLine($"{item.Key} = {item.Value}");
+            }
+            
         }
     }
 }
